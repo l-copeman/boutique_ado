cd84ecc1c64604715030323e88e52c29c853419a
Lee Copeman <leepcopeman@gmail.com>
2024-11-12 17:01:22 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QKM4iDIwIPOm657wuUTu6HgU757tg2ZvVByqYEOrMVuftR0DtzAwtOa2VzHm3HFNLNzMRmLow3YfDFYkbw4EaWr00ddoYUDk9',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
